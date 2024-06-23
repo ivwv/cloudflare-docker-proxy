8bdb3f61d0fd737a4fef3ce56420121bec260e01
SY <99464244+ivwv@users.noreply.github.com>
2024-06-23 14:06:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ivwv.site: dockerHub,
+  "quay.ivwv.site: "https://quay.io",
+  "gcr.ivwv.site: "https://gcr.io",
+  "k8s-gcr.ivwv.site: "https://k8s.gcr.io",
+  "k8s.ivwv.site: "https://registry.k8s.io",
+  "ghcr.ivwv.site: "https://ghcr.io",
+  "cloudsmith.ivwv.site: "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ivwv.site: dockerHub,
 };
 
 function routeByHosts(host) {
